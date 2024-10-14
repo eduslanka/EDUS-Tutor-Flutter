2695f7ab1cb02bc1da47d8870252a3e08b2d9507
Kukeenthan <kukeenthan@miwayz.com>
2024-10-15 02:01:18 +0530

    pusher channel removed

--- lib/screens/student/student_record/presentation/video_player_page.dart
+++ lib/screens/student/student_record/presentation/video_player_page.dart
@@ -1,68 +1,68 @@
-import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
 
-class GoogleDriveVideoPlayer extends StatefulWidget {
-  final String videoUrl;
+// class GoogleDriveVideoPlayer extends StatefulWidget {
+//   final String videoUrl;
 
-  const GoogleDriveVideoPlayer({Key? key, required this.videoUrl})
-      : super(key: key);
+//   const GoogleDriveVideoPlayer({Key? key, required this.videoUrl})
+//       : super(key: key);
 
-  @override
-  _GoogleDriveVideoPlayerState createState() => _GoogleDriveVideoPlayerState();
-}
+//   @override
+//   _GoogleDriveVideoPlayerState createState() => _GoogleDriveVideoPlayerState();
+// }
 
-class _GoogleDriveVideoPlayerState extends State<GoogleDriveVideoPlayer> {
-  late VideoPlayerController _controller;
-  late Future<void> _initializeVideoPlayerFuture;
+// class _GoogleDriveVideoPlayerState extends State<GoogleDriveVideoPlayer> {
+//   late VideoPlayerController _controller;
+//   late Future<void> _initializeVideoPlayerFuture;
 
-  @override
-  void initState() {
-    super.initState();
-    // Initialize the video player controller with the Google Drive URL
-    _controller = VideoPlayerController.network(widget.videoUrl);
-    _initializeVideoPlayerFuture = _controller.initialize();
-    _controller.setLooping(true);
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     // Initialize the video player controller with the Google Drive URL
+//     _controller = VideoPlayerController.network(widget.videoUrl);
+//     _initializeVideoPlayerFuture = _controller.initialize();
+//     _controller.setLooping(true);
+//   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Google Drive Video'),
-      ),
-      body: FutureBuilder(
-        future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Center(
-              child: AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              ),
-            );
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _controller.value.isPlaying
-                ? _controller.pause()
-                : _controller.play();
-          });
-        },
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Google Drive Video'),
+//       ),
+//       body: FutureBuilder(
+//         future: _initializeVideoPlayerFuture,
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.done) {
+//             return Center(
+//               child: AspectRatio(
+//                 aspectRatio: _controller.value.aspectRatio,
+//                 child: VideoPlayer(_controller),
+//               ),
+//             );
+//           } else {
+//             return Center(child: CircularProgressIndicator());
+//           }
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           setState(() {
+//             _controller.value.isPlaying
+//                 ? _controller.pause()
+//                 : _controller.play();
+//           });
+//         },
+//         child: Icon(
+//           _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+//         ),
+//       ),
+//     );
+//   }
+// }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,9 +14,8 @@ import in_app_review
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
-import video_player_avfoundation
 import wakelock_plus
 import webview_flutter_wkwebview
 
@@ -32,7 +31,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
   FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
